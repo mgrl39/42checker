d4974723a71cb56f9bc570b9041e9ad7267f194d
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-23 23:25:43 +0200

    Create colors.h

---
+++ libft/colors.h
@@ -0,0 +1,44 @@
+#iffndef COLORS_H
+#define COLORS_H
+
+// Regular colors
+#define ANSI_COLOR_BLACK   "\x1b[0;30m"
+#define ANSI_COLOR_RED     "\x1b[0;31m"
+#define ANSI_COLOR_GREEN   "\x1b[0;32m"
+#define ANSI_COLOR_YELLOW  "\x1b[0;33m"
+#define ANSI_COLOR_BLUE    "\x1b[0;34m"
+#define ANSI_COLOR_PURPLE  "\x1b[0;35m"
+#define ANSI_COLOR_CYAN    "\x1b[0;36m"
+#define ANSI_COLOR_WHITE   "\x1b[0;37m"
+
+// Bold Colors
+#define ANSI_COLOR_BOLD_BLACK   "\x1b[1;30m"
+#define ANSI_COLOR_BOLD_RED     "\x1b[1;31m"
+#define ANSI_COLOR_BOLD_GREEN   "\x1b[1;32m"
+#define ANSI_COLOR_BOLD_YELLOW  "\x1b[1;33m"
+#define ANSI_COLOR_BOLD_BLUE    "\x1b[1;34m"
+#define ANSI_COLOR_BOLD_PURPLE  "\x1b[1;35m"
+#define ANSI_COLOR_BOLD_CYAN    "\x1b[1;36m"
+#define ANSI_COLOR_BOLD_WHITE   "\x1b[1;37m"
+
+// Background Colors
+#define ANSI_COLOR_BG_BLACK   "\x1b[40m"
+#define ANSI_COLOR_BG_RED     "\x1b[41m"
+#define ANSI_COLOR_BG_GREEN   "\x1b[42m"
+#define ANSI_COLOR_BG_YELLOW  "\x1b[43m"
+#define ANSI_COLOR_BG_BLUE    "\x1b[44m"
+#define ANSI_COLOR_BG_PURPLE  "\x1b[45m"
+#define ANSI_COLOR_BG_CYAN    "\x1b[46m"
+#define ANSI_COLOR_BG_WHITE   "\x1b[47m"
+
+// Reset Color
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+#endif // COLORS_H
+
+// \x1b[0;31m: Se usa típicamente cuando deseas asegurarte de que el
+// texto comience con un formato limpio en términos de atributos 
+// (sin negrita, sin subrayado, etc.) y solo aplique el color especificado.
+
+// \x1b[31m: Puede usarse cuando deseas mantener los atributos de texto existentes 
+// (si los hay) mientras cambias el color.
