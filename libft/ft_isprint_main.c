d98b0cd784beb5216963da217f52fb6b9ed9de72
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-23 23:36:00 +0200

    Update ft_isprint_main.c

--- libft/ft_isprint_main.c
+++ libft/ft_isprint_main.c
@@ -37,12 +37,6 @@ void	ft_putnbr(int nb)
 	ft_putchar(nb + '0');
 }
 
-// Define colors
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
-
 int main(void)
 {
     int c;
