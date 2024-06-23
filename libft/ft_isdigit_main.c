5de6b6190e84b7909660790557a12b63c638a9fa
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-23 18:45:36 +0200

    Update ft_isdigit_main.c

--- libft/ft_isdigit_main.c
+++ libft/ft_isdigit_main.c
@@ -1,48 +1,65 @@
 #include <unistd.h>
 
 // Prototype of ft_isdigit function
-int     ft_isdigit(int c);
+int ft_isdigit(int c);
 
 // Prototype of ft_putchar
-void    ft_putchar(char c)
+void ft_putchar(char c)
 {
-        write(1, &c, 1);
+    write(1, &c, 1);
 }
 
 // Prototype of ft_putstr 
-void    ft_putstr(char *str)
+void ft_putstr(char *str)
 {
-        int     i;
-
-        i = 0;
-        while (str[i] != '\0')
-        {
-                write(1, &str[i], 1);
-                i++;
-        }
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        write(1, &str[i], 1);
+        i++;
+    }
 }
 
-int     main(void)
-{
-        int      c;
-        int     result;
+// Definición de colores
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 
-        c = 0;
-        while (c <= 127) 
+int main(void)
+{
+    int c;
+    int result;
+
+    c = 0;
+    while (c <= 127)
+    {
+        result = ft_isdigit(c);
+        if (((c >= '0' && c <= '9' && result == 0) || (c < '0' || c > '9' && result != 0)))
         {
-                result = ft_isdigit(c);
-                if ((c >= '0' && c <= '9' && result == 0) || ((c < '0' || c > '9') && result != 0)) {
-                        ft_putstr("Error: ft_isdigit('");
-                        ft_putchar(c);
-                        ft_putstr("') returned ");
-                        if (result == 0)
-                                ft_putchar('0');
-                        else
-                                ft_putchar('1');
-                        ft_putstr("\n");
-                }
-                c++;
+            // Imprimir mensaje de error en rojo
+            ft_putstr(ANSI_COLOR_RED "Error: ft_isdigit('");
+            ft_putchar(c);
+            ft_putstr("') returned ");
+            if (result == 0)
+                ft_putchar('0');
+            else
+                ft_putchar('1');
+            ft_putstr(ANSI_COLOR_RESET "\n");
         }
-        ft_putstr("Tests completed.\n");
-        return 0;
+        else
+        {
+            // Imprimir mensaje correcto en verde
+            ft_putstr(ANSI_COLOR_GREEN "Correct: ft_isdigit('");
+            ft_putchar(c);
+            ft_putstr("') returned ");
+            if (result == 0)
+                ft_putchar('0');
+            else
+                ft_putchar('1');
+            ft_putstr(ANSI_COLOR_RESET "\n");
+        }
+        c++;
+    }
+    ft_putstr("Tests completed.\n");
+    return 0;
 }
