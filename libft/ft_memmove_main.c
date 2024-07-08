8b91ba6998e0b6e9b1758497f17763b8986e412b
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2024-07-08 17:02:22 +0200

    Update ft_memmove_main.c

--- libft/ft_memmove_main.c
+++ libft/ft_memmove_main.c
@@ -9,6 +9,35 @@ typedef struct {
     size_t n;
 } TestCase;
 
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*tmp_dest;
+	unsigned char	*tmp_src;
+	size_t			i;
+
+	if (!dest && !src)
+		return (0);
+	tmp_dest = (unsigned char *)dest;
+	tmp_src = (unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		tmp_dest[i] = tmp_src[i];
+		i++;
+	}
+	return (dest);
+}
+
 void	*ft_memmove(void *dest, const void *src, size_t n);
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
