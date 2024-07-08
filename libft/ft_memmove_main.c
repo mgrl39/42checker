20ee4ef186d829e843b42f7df65ae35c6216abfa
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-07-08 16:46:41 +0200

    Update ft_memmove_main.c

--- libft/ft_memmove_main.c
+++ libft/ft_memmove_main.c
@@ -9,18 +9,6 @@ typedef struct {
     size_t n;
 } TestCase;
 
-
-#include <stdio.h>
-#include <string.h>
-#include "libft.h"
-#include "colors.h"
-
-typedef struct {
-    char *dest;
-    const char *src;
-    size_t n;
-} TestCase;
-
 void	*ft_memmove(void *dest, const void *src, size_t n);
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
