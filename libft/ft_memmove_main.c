dbbe243392edd24522569dec7c62c9f844364433
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2024-07-08 16:44:01 +0200

    Update ft_memmove_main.c

--- libft/ft_memmove_main.c
+++ libft/ft_memmove_main.c
@@ -23,6 +23,28 @@ typedef struct {
 
 void	*ft_memmove(void *dest, const void *src, size_t n);
 
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t		len;
+	size_t		i;
+
+	i = 0;
+	if (!src)
+		return (0);
+	len = ft_strlen(src);
+	if (dstsize == 0)
+		return (len);
+	dstsize--;
+	while (dstsize > 0 && src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+		dstsize--;
+	}
+	dst[i] = '\0';
+	return (len);
+}
+/*
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	len;
@@ -42,7 +64,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 		dst[i] = '\0';
 	return (len + ft_strlen(src));
 }
-
+*/
 
 void ft_memmove_test(char *dest, const char *src, size_t n, int *passed, int *failed)
 {
